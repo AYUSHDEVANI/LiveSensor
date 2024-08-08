33340a919c22a427e2c6ee68e501d0315815b7f4
Devani Ayush <ayushdevani0018@gmail.com>
2024-08-08 08:33:15 +0530

    Create ven Virtual Environment and requirements.txt and setup.py Files

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # LiveSensor
-I want to create sendor fault prediction project
+###### I want to create sendor fault prediction project
+
+####    ----Create a Virtual Environmrnt
+```
+conda create -p venv python=3.8 -y
+```
+
+####    ----Run setup.py
+```
+python setup.py install
+```
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements() -> List[str]:
+    requirement_list = List[str] 
+
+    return requirement_list
+
+
+setup(
+    name = 'sensor',
+    version = '0.0.1',
+    author = 'Devani Ayush',
+    author_email = 'ayushdevani0018@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements()
+)
